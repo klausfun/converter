21d3d48c32d1358672d01d9ef1cb54584504367f
Artem <you@example.com>
2023-08-24 00:24:23 +0300

    modification

--- main.py
+++ main.py
@@ -1,17 +1,13 @@
 import pandas as pd
-import openpyxl
-import csv
 
 
 def read():
-    x = pd.read_excel(io='first_project.xlsx', engine='openpyxl')
-    return x
+    return pd.read_excel(io='first_project.xlsx', engine='openpyxl')
 
 
-def write(y):
-    y.to_csv('output_file_name.csv', index=False)  # index=False prevents pandas from writing a row index to the CSV.
+def write(table):
+    table.to_csv('output_file_name.csv', index=False)
 
 
 if __name__ == "__main__":
-    t = read()
-    write(t)
+    write(read())
